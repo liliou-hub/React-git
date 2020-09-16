d0384baf5da35c8953ba3162a76b157252176ed4
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-09-16 17:19:51 +0200

    first commit

---
+++ first-react/src/components/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Title extends components {
+    render() {
+        return (          
+            <h1>Name: {this.props.name}</h1>
+        );
+    }
+}
+
+export default Title;
