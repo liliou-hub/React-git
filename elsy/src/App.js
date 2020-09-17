3965a09ee58caf3ac145d5f2755371f73510ca00
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-09-17 16:52:25 +0200

    premier jour elsy

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Person from './component/Person.js';
+
+const MIN_TEMPERATURE = -20
+const MAX_TEMPERATURE = 40
+const MIN_HEART = 80
+const MAX_HEART = 180
+const MIN_STEPS = 0
+const MAX_STEPS = 50000
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app'>Bonjour!
+        <div className='fluid-container'>
+          <p>Battements de coeur: 80</p>
+          <p>Température : -20</p>
+          <p>Nombre de pas : 0</p>          
+        </div>
       </div>
     );
   }
---
+++ elsy/src/components/Person.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class Person extends Component {
+  render() {
+    return (
+     <div className= 'box col-md-2 col-6'>
+         <span class="material-icons" style='fontSize=100 color=black'>
+directions_walk
+</span>
+     </div>
+    );
+  }
+}
+
+export default Person;
