0483ca769911702da2c9e023906481c0671ba8b1
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-09-18 00:22:44 +0200

    elsy etape3

--- elsy/src/components/Person.js
+++ elsy/src/components/Person.js
@@ -5,8 +5,8 @@ import React, { Component } from 'react';
 class Person extends Component {
     render() {
         return (
-            <div className='box col-md-2 col-6'>
-                <span class="material-icons" style={{ fontSize: '100', color: 'Black' }}>
+            <div className='box col-md-2 col-6' style={{ fontSize: '100', color: 'black' }}>
+                <span class="material-icons">
                 {/* directions_walk */}
                     {this.props.icon}
                 </span>
