20b36464f969cb7bf0cfb644df319f2942be1b73
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-09-22 16:09:41 +0200

    ME-Counter étape 1 et 2 finies

--- react_counter/src/App.js
+++ react_counter/src/App.js
@@ -1,42 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+/*ETAPE 2 voir étape 1 tout en bas*/
+
+
+import React, { Component } from "react";
 import './App.css';
-import Counter from './components/Counter.js'
+import Counter from './components/Counter'
 
 class App extends Component {
 
   constructor() {
     super()
-    this. incrementCount = this.incrementCount.bind(this)
-    this. decrementCount = this.decrementCount.bind(this)
+    this.incrementCount = this.incrementCount.bind(this)
+    this.decrementCount = this.decrementCount.bind(this)
   }
 
-  state={
-    count:1 
-  }
+  state = {
+    count: 1,
+  };
 
-  incrementCount(){
+  incrementCount() {
     this.setState({
-     counter: this.state.count+1
-    });
-}
+      count: this.state.count + 1
+    })
+  }
 
-  decrementCount(){
-  this.setState({
-   counter: this.state.count-1
-  });
-}
-  
+  decrementCount() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-          <h1>Counter</h1>
-          <Counter counter={this.state.count}
-                   incrementFn={this.incrementCount}
-                   decrementFn={this.decrementCount}></Counter>
-       </div>
+      <div className='App'>
+        <h1>Counter</h1>
+        <Counter incrementFn={this.incrementCount}
+          decrementFn={this.decrementCount}
+          count={this.state.count}></Counter>
+      </div>
     );
   }
 }
-
 export default App;
+
+
+
+
+/*ETAPE 1*/
+
+// import React, { Component } from "react";
+// import './App.css';
+
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super()
+//     this.incrementCount=this.incrementCount.bind(this)
+//     this.decrementCount=this.decrementCount.bind(this)
+//   }
+
+//   state = {
+//     count: 1,
+//   };
+
+//   incrementCount () {
+//     this.setState ({
+//       count : this.state.count + 1
+//     })
+//   }
+
+//   decrementCount () {
+//     this.setState ({
+//       count : this.state.count - 1
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Counter</h1>
+//         <h2>{this.state.count}</h2>
+//          <button  
+//          style={{backgroundColor: "green"}}
+//           onClick= {this.incrementCount} > + </button>
+//          <button
+//           style={{backgroundColor: "red"}}
+//           onClick={this.decrementCount} > - </button> 
+//       </div>
+//     );
+//   }
+// }
+// export default App;
--- react_counter/src/components/Counter.js
+++ react_counter/src/components/Counter.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 
 
-
-
 class Counter extends Component {
+
 
     render() {
         return (
             <div>
-                <h2>{this.props.count}</h2>        
-                <button onClick={this.props.incrementCountFn} style={{background:'green'}}>+</button>  
-                <button onClick={this.props.decrementCountFn} style={{background:'red'}}>-</button>   
+                <h2>{this.props.count}</h2>
+                <button onClick={this.props.incrementFn} style={{ backgroundColor: "green" }} > + </button>
+                <button onClick={this.props.decrementFn} style={{ backgroundColor: "red" }}> - </button>
             </div>
         );
     }
