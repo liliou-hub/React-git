d3e4a7037699ca370c26ac132647c143b2de4c74
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-09-25 16:57:56 +0200

    Bakery etape 1

--- react_bakery/src/App.js
+++ react_bakery/src/App.js
@@ -5,15 +5,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Pay from './components/Pay'
 import Add from './components/Add'
 import List from './components/List'
+import Button from './components/core/Button'
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this) 
+
+    this.state = {
+      activeTab:Add,
+      items:[]
+    }
+  }
+
+  handleClick(Add){
+    this.setState({
+      activeTab:Add
+    })
+  
+
+  }
+
 
   render() {
     return (
       <div className="App">
-            activeTab={this.state.Add}
-              items={[]}
+       <Add></Add>
+       <Pay></Pay>
+       <List></List>   
+       <div className="row justify-content-center">
+       <Button onClick ={this.handleClick}  isSelected={this.state.isSelected} children='Add'/>
+       <Button onClick={this.handleClick}  isSelected={this.state.isSelected} children='List'/>
+       <Button onClick={this.handleClick} isSelected={this.state.isSelected}  children='Pay'/>
+       </div>
+       
       </div>
+
+
 
     );
   }
--- react_bakery/src/components/core/Button.js
+++ react_bakery/src/components/core/Button.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 
 
+
 class Button extends Component {
     render() {
         return (
             <div>
-                {this.props.children}
-                {this.props.isSelected}
-                {this.props.onClick}
+                <button className="btn btn-primary"  onClick={this.props.handleClick} isSelected={this.props.isSelected}> {this.props.children}</button>            
             </div>
         );
     }
 }
 
 export default Button;
+
