ba9ee22e952376ed37a03a696ebb754e872a27ec
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-10-02 16:51:26 +0200

    etape2

--- react_country/src/App.js
+++ react_country/src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/bootstrap.min.css'
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.state = {
+      name: '',
+      capital: '',
+      flag: '',
+      population:  0,
+      region: ''
+    }
+
+  };
+
+
+  componentDidMount() {
+    fetch('https://restcountries.eu/rest/v2/name/france')
+      .then(res => res.json())
+      .then(json => {
+        // code
+
+      });
+
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='App-header'>
+Country Selector
+        </div>
+        <p>{this.state.name}</p>
+        <p>{this.state.capital}</p>
+        <p>{this.state.flag}</p>
+        <p>{this.state.population}</p>
+        <p>{this.state.region}</p>
       </div>
+
     );
+
   }
+
 }
 
 export default App;
